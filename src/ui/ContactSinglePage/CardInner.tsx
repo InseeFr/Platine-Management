00f0459f486b426ad53818a69b3be498a737f80e
Lea Renaux <lea.renaux@insee.fr>
2024-01-30 09:30:27 +0100

    add DeleteCommentDialog

--- src/ui/ContactSinglePage/CardInner.tsx
+++ src/ui/ContactSinglePage/CardInner.tsx
@@ -1,25 +1,54 @@
-import { Card, IconButton } from "@mui/material";
-import { ReactNode } from "react";
+import { Box, Card, IconButton, Tooltip } from "@mui/material";
+import { ReactNode, useState } from "react";
 import { Row } from "../Row";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { DeleteCommentDialog } from "./DeleteCommentDialog";
 
 type Props = {
   content: ReactNode;
   hasAction?: boolean;
+  hasTooltip?: boolean;
+  tooltipContent?: ReactNode;
   onDelete?: () => void;
 };
 
-export const CardInner = ({ content, hasAction, onDelete }: Props) => {
+//TODO: tooltip style
+
+export const CardInner = ({ content, hasAction, hasTooltip, onDelete, tooltipContent }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card sx={{ px: 2, py: 2, backgroundColor: "#EBEFF5" }} elevation={0}>
       <Row spacing={1} justifyContent={"space-between"}>
-        {content}
+        {hasTooltip ? (
+          <Tooltip title={tooltipContent} placement="left" arrow>
+            <Box>{content}</Box>
+          </Tooltip>
+        ) : (
+          content
+        )}
         {hasAction && (
-          <IconButton sx={{ color: "inherit" }} aria-label="delete" onClick={onDelete}>
+          <IconButton sx={{ color: "inherit" }} aria-label="delete" onClick={handleClickOpen}>
             <DeleteOutlineIcon fontSize="small" />
           </IconButton>
         )}
       </Row>
+      <DeleteCommentDialog
+        open={open}
+        handleClose={handleClose}
+        onDelete={() => {
+          onDelete && onDelete();
+          handleClose();
+        }}
+      />
     </Card>
   );
 };
