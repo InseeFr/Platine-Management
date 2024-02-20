44fdbcdbf2ce96a26f80df7609c5a29adc8ab6c1
Eric Thuaud <eric.thuaud@insee.fr>
2024-02-20 21:43:49 +0100

    feat - implem settingsHabilitations table and simplify with tablepagination

--- src/ui/Settings/SettingsHabilitationsCard.tsx
+++ src/ui/Settings/SettingsHabilitationsCard.tsx
@@ -13,10 +13,9 @@ import {
   IconButton,
   TextField,
   CircularProgress,
-  Select,
-  MenuItem,
-  OutlinedInput,
   Button,
+  TableFooter,
+  TablePagination,
 } from "@mui/material";
 import { useInfiniteFetchQuery } from "../../hooks/useFetchQuery";
 import { SettingsHabilitationsMenu } from "./SettingsHabilitationsMenu";
@@ -24,31 +23,48 @@ import { RoleChip } from "./RoleChip";
 import { Row } from "../Row";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { APISchemas } from "../../types/api";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import { format } from "date-fns";
 
 const endpoint = "/api/users" as const;
-
-const roles = ["administrateur", "responsable", "gestionnaire", "assistance"];
 
 export const SettingsHabilitationsCard = () => {
   const {
     results: users,
-    hasNextPage,
+    /*   hasNextPage,
     hasPreviousPage,
     fetchNextPage,
-    fetchPreviousPage,
+    fetchPreviousPage, */
   } = useInfiniteFetchQuery(endpoint);
   const [searchList, setSearchList] = useState<Array<APISchemas["UserDto"]>>([]);
+  const [rowsPerPage, setRowsPerPage] = useState(7);
+  const [pageNumber, setPageNumber] = useState<number>(0);
+
+  useEffect(() => {
+    setSearchList(users);
+  }, [users]);
+
+  const handleChangePage = (_: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPageNumber(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPageNumber(0);
+  };
 
   function filterValues(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void {
     const filteredList = users.filter(u =>
-      e.target.value.length > 0 ? u.identifier.includes(e.target.value) : u,
+      e.target.value.length > 0
+        ? u.identifier.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          u.name?.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          u.role?.toLowerCase().includes(e.target.value.toLowerCase())
+        : u,
     );
-
-    console.log(filteredList);
     setSearchList(filteredList);
+    setPageNumber(0);
   }
   if (!users) {
     return (
@@ -69,18 +85,6 @@ export const SettingsHabilitationsCard = () => {
         <Row justifyContent={"space-between"}>
           <Row spacing={2}>
             <TextField label="Rechercher dans le tableau" onChange={e => filterValues(e)} />
-            <Select
-              size="medium"
-              displayEmpty
-              sx={{ width: "200px" }}
-              input={<OutlinedInput size="small" />}
-            >
-              {roles.map(r => (
-                <MenuItem key={r} value={r}>
-                  <RoleChip role={r.toLowerCase()} />
-                </MenuItem>
-              ))}
-            </Select>
           </Row>
 
           <Button
@@ -114,15 +118,22 @@ export const SettingsHabilitationsCard = () => {
                 <TableCell align="center" sx={{ fontWeight: 700 }}>
                   Droits Pilotage
                 </TableCell>
-                <TableCell align="center">Droits annuaire</TableCell>
-                <TableCell align="center">Source</TableCell>
-                <TableCell align="center">Unité enquêtée</TableCell>
-                <TableCell align="center">Actions</TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700 }}>
+                  Droits annuaire
+                </TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700 }}>
+                  Source
+                </TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700 }}>
+                  Unité enquêtée
+                </TableCell>
+                <TableCell align="center" sx={{ fontWeight: 700 }}>
+                  Actions
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {searchList.length} &&
-              {searchList.map(user => (
+              {searchList.slice(rowsPerPage * pageNumber, rowsPerPage * (pageNumber + 1)).map(user => (
                 <TableRow key={user.identifier}>
                   <TableCell align="center">{user.identifier}</TableCell>
                   <TableCell align="center">{user.name}</TableCell>
@@ -133,36 +144,50 @@ export const SettingsHabilitationsCard = () => {
                   </TableCell>
                   <TableCell align="center">{"not provided"}</TableCell>
                   <TableCell align="center">{"not provided"}</TableCell>
-                  <TableCell align="center">{user.accreditedSources}</TableCell>
-                  <TableCell align="center">{`Le "${user.creationDate}" par "${user.creationAuthor}`}</TableCell>
+                  <TableCell align="center">{user.accreditedSources?.join()}</TableCell>
+                  <TableCell align="center">
+                    {user.creationDate ? `Le ${format(user.creationDate, "dd/MM/yyyy")}` : ""}{" "}
+                    {`par 
+                    ${user.creationAuthor}`}
+                  </TableCell>
                   <TableCell align="center">
                     <SettingsHabilitationsMenu user={user} />
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  sx={{
+                    ".MuiTablePagination-displayedRows": {
+                      typography: "bodySmall",
+                    },
+                    ".MuiTablePagination-input": {
+                      typography: "bodySmall",
+                    },
+                    ".MuiTablePagination-selectLabel": {
+                      typography: "bodySmall",
+                      color: "text.tertiary",
+                    },
+                  }}
+                  rowsPerPageOptions={[10, 20, 50]}
+                  labelRowsPerPage={"Lignes par page :"}
+                  labelDisplayedRows={page =>
+                    `${page.from}-${page.to === -1 ? page.count : page.to} sur ${
+                      page.count
+                    } entités affichées`
+                  }
+                  count={searchList.length}
+                  rowsPerPage={rowsPerPage}
+                  page={pageNumber}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
         </TableContainer>
-        <Row justifyContent={"right"}>
-          <Typography>
-            {}
-            {"sur entités affichées"}
-          </Typography>
-          <IconButton
-            sx={{ bgcolor: "background.default" }}
-            disabled={!hasPreviousPage}
-            onClick={() => fetchPreviousPage()}
-          >
-            <ChevronLeftIcon sx={{ color: "black.main" }} />
-          </IconButton>
-          <IconButton
-            sx={{ bgcolor: "background.default" }}
-            disabled={!hasNextPage}
-            onClick={() => fetchNextPage()}
-          >
-            <ChevronRightIcon sx={{ color: "black.main" }} />
-          </IconButton>
-        </Row>
       </Stack>
     </Card>
   );
